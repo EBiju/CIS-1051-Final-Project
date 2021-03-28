201479c207edfdbd5341d51ef9acf2655d0ef907
EBiju <81535461+EBiju@users.noreply.github.com>
2021-03-28 17:23:24 -0400

    Add files via upload

---
+++ Final Project Preposal.py
@@ -0,0 +1,14 @@
+# Preproposal
+
+## What idea(s) do you have for your final project?
+
+I planning on making a simple side scrolling game, similar to mario or megaman.
+## If you plan to collaborate with one or two classmates, what are their names?
+
+I will be working by myself.
+
+## Do you have any questions of your own?
+
+I need to first decide what to code the game in,
+how much will I have to be accountable for when writing the game's code,
+and what ideas I can use to create it.
