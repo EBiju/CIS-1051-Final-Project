f86656e7e2ecca70457507615d968aa73a06ad56
EBiju <81535461+EBiju@users.noreply.github.com>
2021-04-10 23:30:42 -0400

    Add files via upload

---
+++ ProjectProposal.py
@@ -0,0 +1,42 @@
+# Proposal
+
+## What will (likely) be the title of your project?
+
+Nostalgia Platformer
+
+## In just a sentence or two, summarize your project. (E.g., "A website that lets you buy and sell stocks.")
+
+Using Lua, I will be making a platforming game which will be similar to eight bit games of the 80s.
+
+## In a paragraph or more, detail your project. What will your software do? What features will it have? How will it be executed?
+I want to make a single level of a platformer that will be similar to an nintendo game Megaman.
+I am looking to implement enemies and different ways of platforming like moving
+platforms. I will be programing this game on Lua as I am currently trying to complete the Mario path
+from Harvard university. 
+
+
+
+## If planning to collaborate with 1 or 2 classmates for the final project, list their names, email addresses, and the names of their assigned TAs below.
+
+
+## In the world of software, most everything takes longer to implement than you expect. And so it's not uncommon to accomplish less in a fixed amount of time than you hope.
+
+### In a sentence (or list of features), define a GOOD outcome for your final project. I.e., what WILL you accomplish no matter what?
+
+I am able to make the level fully functional and have my character move throughout the stage and jump.
+
+### In a sentence (or list of features), define a BETTER outcome for your final project. I.e., what do you THINK you can accomplish before the final project's deadline?
+
+I can perfectly implement enemies and give the character a weapon to shoot said enemies. 
+
+### In a sentence (or list of features), define a BEST outcome for your final project. I.e., what do you HOPE to accomplish before the final project's deadline?
+
+I am able to add a boss at the end of the level, similar to the megaman games, and make it beatable.
+
+## In a paragraph or more, outline your next steps. What new skills will you need to acquire? What topics will you need to research? If working with one of two classmates, who will do what
+
+Using my knowledge from the Mario project, I will build off of it to create this game using different paths and designs.
+I will need a better grasp of Lua as I have only had a little time to work with it in the first place. I will need to first
+create the sprites for the characters, enemies, platforms, and other elements within it. Then I would need to piece the each of
+the sprites together individually and create the level. If I am able to, I will create a 'boss' of some sort along with a health bar
+to indicate a players damage taken.
